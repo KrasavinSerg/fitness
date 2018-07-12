38548f09135f8932f447155a02bb0c8fe84d9646
KrasavinSerg <krasavin.serg@gmail.com>
2018-07-12 14:28:27 +0300

    added project

---
+++ js/clock.js
@@ -0,0 +1,20 @@
+var clock = $('.clock').FlipClock({
+  // clockFace : 'DailyCounter',
+  autoStart : false,
+  language:'ru-ru',
+  callbacks : {
+    stop : function () {
+      $('.message').html('Время истекло');
+    }
+  }
+});
+
+var dt = ('2018/07/12 23:59:59'); //Дата до которой нужен таймер
+var first = new Date(dt);
+var last = Date.now();
+var remaining = first - last;
+remaining /= 1000;
+
+clock.setTime(remaining);
+clock.setCountdown(true);
+clock.start();
---
+++ js/main.js
@@ -0,0 +1,43 @@
+var offer = document.querySelector('#top__btn');
+var modal1 = document.querySelector('#modal1');
+var modal2 = document.querySelector('#modal2');
+var close1 = document.querySelector('#close1');
+var close2 = document.querySelector('#close2');
+var address = document.querySelector('#address');
+
+
+offer.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal1.classList.add('modal-show');
+});
+
+close1.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal1.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal1.classList.contains('modal-show')) {
+      modal1.classList.remove('modal-show');
+    }
+  }
+});
+
+address.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal2.classList.add('modal-show');
+});
+
+close2.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal2.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal2.classList.contains('modal-show')) {
+    modal2.classList.remove('modal-show');
+    }
+  }
+});
