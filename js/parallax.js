9bcf8f561cfc0a6bfe61eb9d95614bd3499b29fe
KrasavinSerg <krasavin.serg@gmail.com>
2018-07-15 21:30:26 +0300

    corrected parallax

--- js/parallax.js
+++ js/parallax.js
@@ -1,15 +1,6 @@
 $(function () {
-  $('.offer-top__wrap-left').on('mousemove', function (e) {
-    $('.first').css({
-      left: -e.pageX / 50 + 1,
-      top: -e.pageY / 50 + 1
-    });
-  });
-});
-
-$(function () {
   $('.offer-top__wrap-right').on('mousemove', function (e) {
-    $('.second').css({
+    $('.parallax').css({
       left: e.pageX / 50 + 1,
       top: e.pageY / 50 + 1
     });
