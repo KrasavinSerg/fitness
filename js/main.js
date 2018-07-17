fe3397969785e1ef3dfef894fb3826b4a5c82d8e
KrasavinSerg <krasavin.serg@gmail.com>
2018-07-17 16:57:21 +0300

    small changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 var offer = document.querySelector('#top__btn');
 var modal1 = document.querySelector('#modal1');
 var modal2 = document.querySelector('#modal2');
-// var modal3 = document.querySelector('#modal3');
 var close1 = document.querySelector('#close1');
 var close2 = document.querySelector('#close2');
-// var close3 = document.querySelector('#close3');
 var address = document.querySelector('#address');
 var address_bottom = document.querySelector('#address_bottom');
-// var video = document.querySelector('#video');
 
 
 offer.addEventListener('click', function (evt) {
@@ -63,21 +60,3 @@ window.addEventListener('keydown', function (evt) {
     }
   }
 });
-
-// video.addEventListener('click', function (evt) {
-//   evt.preventDefault();
-//   modal3.classList.add('modal-show');
-// });
-
-// close3.addEventListener('click', function (evt) {
-//   evt.preventDefault();
-//   modal3.classList.remove('modal-show');
-// });
-
-// window.addEventListener('keydown', function (evt) {
-//   if (evt.keyCode === 27) {
-//     if (modal3.classList.contains('modal-show')) {
-//       modal3.classList.remove('modal-show');
-//     }
-//   }
-// });
